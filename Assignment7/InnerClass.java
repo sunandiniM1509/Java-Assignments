00312a8dbb0421446eb2a7c21333099f08620982
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-05 22:04:03 +0530

    Update InnerClass.java

--- Assignment7/InnerClass.java
+++ Assignment7/InnerClass.java
@@ -1,5 +1,5 @@
-//Create a class with an inner class that has a non-default constructor (one that takes arguments). Create a second class
-// with an inner class that inherits from the first inner class.
+/**A Base class with an inner class that has a non-default constructor 
+*Another class with an inner class that inherits from the first inner class.*/
 class Base1
 {
     class Inner1
