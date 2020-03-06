b113cb2d752c2bb87dddab4b828c91a2b72e57bd
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-06 12:23:08 +0530

    Update CountOccurrences.java

--- Assignment11/CountOccurrences.java
+++ Assignment11/CountOccurrences.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.*;
 
+/**
+*Accepting a text file (input.txt) as an input through command line argument and this program counts the 
+*occurrence of all the different characters and saves the result in a text file (output.txt).
+*/
 class CountOccurrences
 {
     public static void main(String args[]) throws Exception
@@ -8,9 +12,11 @@ class CountOccurrences
         try {
             File file = new File(args[0]);
             int fpointer = 0;
+            //A HashMap to store each character and count of its occurrence.
             HashMap<Character, Integer> CharacterCount = new HashMap<Character, Integer>();
             FileReader freader = new FileReader(file);
-            //file pointer == -1 --> not a valid index, end of the file
+            //traverse through the Map and check for characters
+            //if file pointer == -1 --> not a valid index, end of the file.
             while ((fpointer = freader.read()) != -1) {
                 char ch = (char) fpointer;
                 if (ch != ' ' && ch != '\t' && ch != '\n') {
