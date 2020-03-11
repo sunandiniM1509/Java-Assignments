072fd8ca08ca07b4da8f18852ca7614a8a9ad6b0
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 22:50:47 +0530

    Update InnerClass.java

--- Assignment7/InnerClass.java
+++ Assignment7/InnerClass.java
@@ -1,35 +1,35 @@
-/**A Base class with an inner class that has a non-default constructor 
-*Another class with an inner class that inherits from the first inner class.*/
+/**
+ * A Base class with an inner class that has a non-default constructor is generated
+ *Another class with an inner class that inherits from the first inner class
+ */
 class Base1
 {
     class Inner1
     {
-        Inner1(String s)
+        Inner1(String string)
         {
-            System.out.println(s);
+            System.out.println(string);
         }
     }
 }
-
 class Base2
 {
-   // Base2(String s)
-   // {
-    //    Inner2 obj = new Inner2(s);
-  //  }
+    Base2(String string)
+     {
+       Inner2 obj = new Inner2(string);
+      }
     class Inner2 extends Base1.Inner1
     {
-        Inner2(String s)
+        Inner2(String string)
         {
-            new Base1().super(s);
+            new Base1().super(string);
         }
     }
 }
-
 class InnerClass
 {
     public static void main(String args[])
     {
-      //  new Base2("Inner Class reference ");
+        //  new Base2("Inner Class reference ");
     }
 }
