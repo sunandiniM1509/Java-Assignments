89c9a5bb13b35a9acf8991cc3179f9f945e32383
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 10:52:29 +0530

    Update Ping.java

--- Assignment3/Ping.java
+++ Assignment3/Ping.java
@@ -3,31 +3,37 @@ import java.io.*;
 import java.util.regex.Pattern;
 public class Ping
 {
-    //function that will ping any host ( given as input ) and computes the median of the time taken to ping
-    public static void storePingResponse(String command) throws IOException {
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            ArrayList<Float> pingtime = new ArrayList<>();
-            String response="";
-            while ((response = reader.readLine()) != null)
-            {
-                if(Pattern.matches(".*time=.*",response))
-                {
-                    //  example response : "64 bytes from maa03s26-in-f14.1e100.net (172.217.167.142): icmp_seq=3 ttl=57 time=18.6 ms"
-                    String segments[] = response.split(" ");
-                    //[time=18.6] to 18.6
-                    String pingSeconds = segments[segments.length-2].substring(5);
-                    System.out.println(response);
-                    pingtime.add(Float.parseFloat(pingSeconds));
+
+        //function that will ping any host ( given as input ) and computes the median of the time taken to ping
+        public static void storePingResponse (String command) throws IOException {
+            try {
+                Process process = Runtime.getRuntime().exec(command);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                ArrayList<Float> pingtime = new ArrayList<>();
+                String response = "";
+                while ((response = reader.readLine()) != null) {
+                    if (Pattern.matches(".*time=.*", response)) {
+                        //  example response : "64 bytes from maa03s26-in-f14.1e100.net (172.217.167.142): icmp_seq=3 ttl=57 time=18.6 ms"
+                        String segments[] = response.split(" ");
+                        //[time=18.6] to 18.6
+                        String pingSeconds = segments[segments.length - 2].substring(5);
+                        System.out.println(response);
+                        pingtime.add(Float.parseFloat(pingSeconds));
+                    }
                 }
-            }
-            Collections.sort(pingtime);
+                Collections.sort(pingtime);
                 int length = pingtime.size();
-                if(length % 2 == 0)
+                if (length % 2 == 0)
                     //Median of the pingtime ArrayList
-                    System.out.println("Median of the time taken to ping = "+(pingtime.get(length/2) + pingtime.get(length/2-1))/2+" ms");
+                    System.out.println("Median of the time taken to ping = " + (pingtime.get(length / 2) + pingtime.get(length / 2 - 1)) / 2 + " ms");
                 else
-                    System.out.println("Median of the time take to ping = "+pingtime.get(length/2)+" ms");
+                    System.out.println("Median of the time take to ping = " + pingtime.get(length / 2) + " ms");
+            }
+
+            catch (Exception exception)
+            {
+                exception.printStackTrace();
+            }
     }
     public static void main(String[] args) throws IOException {
         Scanner scanner= new Scanner(System.in);
