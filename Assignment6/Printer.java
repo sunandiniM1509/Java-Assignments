5d57557d1f0904769688e54e42e693f1c0d9a6ec
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 22:06:18 +0530

    Update Printer.java

--- Assignment6/Printer.java
+++ Assignment6/Printer.java
@@ -1,3 +1,7 @@
+/**
+*A class with a constructor that takes a String argument and during construction it prints the argument. 
+*an array of object references to this class are created but objects are not created to assign into the array. 
+*/
 class Print
 {
     String s;
@@ -8,14 +12,11 @@ class Print
     }
 
 }
-
 class Printer
 {
     public static void main(String args[])
     {
-    	// array of references to the class.
         Print printerobjects[]  = new Print[10];
         // No, the initialization messages from the constructor calls are not printed.
-
     }
 }
