fa789d0212e594a191d9c274b21d7abf82d2b5bc
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 14:22:41 +0530

    Update RegexClass.java

--- Assignment1/RegexClass.java
+++ Assignment1/RegexClass.java
@@ -1,13 +1,20 @@
 import java.util.*;
 import java.io.*;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
-/**
- *A program to search through the home directory and look for files that match given regular expression.
- *It takes inputs repeatedly based on number of times to search and prints out the full absolute path
- *of the matching files found and implemented Exception Handling to handle null pointer Exception.
- */
-public class RegexClass{
-
+public class untitled{
+    /**
+     *A program to search through the home directory and look for files that match given regular expression.
+     *It takes inputs repeatedly based on number of times to search and prints out the full absolute path
+     *of the matching files found and handles null pointer exceptions.
+     * @param regex String that holds the regular expression to match
+     * @param filePath String to search for files in that filepath directory
+     * @throws NullPointerException if an empty file is found
+     * @param file variable to hold a file
+     * @param allFiles is an array of files that are present in directory
+     * @param fileName variable to hold name of the file
+     */
+    private static Logger logger = Logger.getLogger(untitled.class.getName());
     public static void checkForFiles(String regex, String filePath) {
         try {
             File file = new File(filePath);
@@ -17,40 +24,32 @@ public class RegexClass{
             if(allFiles == null)
                 return;
             for (File isFile : allFiles) {
-                // if we find a directory call the method with the subDirectory name and same regular expression
                 if (isFile.isDirectory())
                 {
-                    // if(isFile.list().length>-1) {
                     checkForFiles(regex, isFile.getAbsolutePath());
-                    //  }
                 }
                 else {
-                    //If its a file we print the absolute path
                     String fileName = isFile.getName();
                     if (Pattern.matches(regex, fileName))
-                        System.out.println(isFile.getAbsolutePath());
+                        logger.info(isFile.getAbsolutePath());
                 }
             }
         }
-        //handle the empty file/directory's NullPointerException
         catch (NullPointerException exception) {
             exception.printStackTrace();
-            System.out.println("NullPointerException Caught!");
+            logger.info("NullPointerException Caught!");
         }
     }
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the regular expression :");
-        String regex = scanner.nextLine();// for example : ".*/*.java"
+        logger.info("Enter the regular expression :");
+        String regex = scanner.nextLine();
         String filepath ="/home";
-        System.out.println("Enter how many times to run :");
-        //This helps to take input recursively and stop after several times, in this way it avoid StackOverFlow
+        logger.info("Enter how many times to run :");
         int times = scanner.nextInt();
         while (times-- >0) {
-            //check for files in "filepath" directory
             checkForFiles(regex, filepath);
         }
     }
-
 }
