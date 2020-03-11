c420f9cf601c45f754b0ad118c5a550428ea5bec
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 21:58:19 +0530

    Update VampNumbers.java

--- Assignment6/VampNumbers.java
+++ Assignment6/VampNumbers.java
@@ -1,22 +1,33 @@
 import java.util.*;
+import java.util.logging.Logger;
 
+/**
+ *A java program to print first 100 vampire numbers.
+ */
 class VampNumbers {
-    // Print first 100 Vampire Numbers
-    
-    //Function to return length of Number
+    private static Logger logger = Logger.getLogger(RegexClass.class.getName());
+    /**
+     * A function used to check the length of the number passed
+     * @param n long to hold the number
+     * @return int value which is length of the number n
+     */
     public static int digitLength(long n)
     {
         return Long.toString(n).length();
     }
-
+    /**
+     * Check if a number n having factors a,b is a vampire number or not
+     * @param n long to hold the number
+     * @param a first factor of n
+     * @param b second factor of n
+     * @return true or false based on conditions provided
+     */
     public static boolean checkforVampire(long n, long a, long b)
     {
-    	//check for trailing zeros
-        if(Long.toString(a).endsWith("0") && Long.toString(b).endsWith("0")) 
+        if(Long.toString(a).endsWith("0") && Long.toString(b).endsWith("0"))
             return false;
-        // check for size = length / 2
         int length = digitLength(n);
-        if(digitLength(a) != length / 2 || digitLength(b) != length / 2) 
+        if(digitLength(a) != length / 2 || digitLength(b) != length / 2)
             return false;
         char[] actual = Long.toString(n).toCharArray();
         char[] real = (Long.toString(a)+Long.toString(b)).toCharArray();
@@ -24,31 +35,28 @@ class VampNumbers {
         Arrays.sort(real);
         return Arrays.equals(actual, real);
     }
-
     public static void main(String[] args)
     {
         List<Long> vampireNumbersList = new ArrayList<Long>();
         int j=1;
-        //ArrayList to store the results
         for(long i=1260; vampireNumbersList.size()<100; i++)
         {
-            if((digitLength(i) % 2) == 0) 
+            if((digitLength(i) % 2) == 0)
             {
-            	// O( root N ) complexity to find factors
-            for(long a=2; a<=Math.sqrt(i); a++){
+                for(long a=2; a<=Math.sqrt(i); a++){
 
-                if(i%a==0)
-                {
-                    long b= i/a;
-                    if(checkforVampire(i,a,b) && a<=b)
+                    if(i%a==0)
                     {
-                        vampireNumbersList.add(i);
-                        System.out.println(j++ +" "+i);
-                        break;
+                        long b= i/a;
+                        if(checkforVampire(i,a,b) && a<=b)
+                        {
+                            vampireNumbersList.add(i);
+                            logger.info("The "+ j++ +" vampire number is " +i);
+                            break;
+                        }
                     }
                 }
             }
-        }
         }
     }
 }
