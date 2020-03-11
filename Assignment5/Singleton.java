1b29a6ad986f9c025b243de363ae2cd17fd8616a
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 21:39:30 +0530

    Update Singleton.java

--- Assignment5/Singleton.java
+++ Assignment5/Singleton.java
@@ -1,19 +1,22 @@
 package sunandini.assignment.singleton;
-/**Added a static method that takes String as parameter which return object of that class. 
-Added a non static method to print the String.*/
-
 public class Singelton
 {
+  private static Logger logger = Logger.getLogger(RegexClass.class.getName());
   public String str;
-  
+/**
+*Adding a static method that takes String as parameter which return object of that class. 
+*/
   public static Singelton initString(String str)
   {
     Singelton sn = new Singelton();
     sn.str = str;
     return sn;
   }
+/**
+*Adding a non static method to print the String.
+*/
   public void print()
   {
-   System.out.println(str);
+   logger.info(str);
   }
 }
