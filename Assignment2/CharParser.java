56b7503720e575c893923042e37ea184735e5e60
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 21:22:53 +0530

    Update CharParser.java

--- Assignment2/CharParser.java
+++ Assignment2/CharParser.java
@@ -1,16 +1,19 @@
 import java.util.*;
+import java.util.logging.Logger;
+
 public class CharParser {
+    private static Logger logger = Logger.getLogger(RegexClass.class.getName());
     /**
-    *A java function to check if the input string contains all the letters of the alphabet a-z (case-insensitive)
-    *@param input String to hold user provided input
-    */
+     *A java function to check if the input string contains all the letters of the alphabet a-z (case-insensitive)
+     *@param input String to hold user provided input
+     */
     static boolean checkCharacter(String input)
     {
         if(input.length()<26)
             return false;
         int[] countchar = new int[26];
         int length = input.length();
-        
+
         for(int i=0;i<length;i++)
             countchar[input.charAt(i)-'a']++;
         for(int j=0;j<26;j++)
@@ -21,15 +24,16 @@ public class CharParser {
         return true;
     }
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    String input =scanner.nextLine();
-    if(checkCharacter(input))
-    System.out.println("Yes, The given input String consists of all the {a-z} characters");
+        Scanner scanner = new Scanner(System.in);
+        String input =scanner.nextLine();
+        if(checkCharacter(input))
+            logger.info("Yes, The given input String contain all the {a-z} characters");
         else
-    System.out.println("No, The given input String does not consists of all the {a-z} characters");        
+            logger.info("No, The given input String does not contain all the {a-z} characters");
     }
 }
 /**
-*The algorithm has Linear Time Complexity of O(n) where n is the length of the string.
-*Space Complexity is O(26) as a constant array of size 26 (count lower case alphabets ) is required to store occurrences of a-z.
-*/
+ *The algorithm has Linear Time Complexity of O(n) where n is the length of the string.
+ *Space Complexity is O(26) as a constant array of size 26 (count lower case alphabets ) is
+ *required to store occurrences of a-z.
+ */
