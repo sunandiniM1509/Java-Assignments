1cf503547f52bf237fc06a7a047ee9d0688e814a
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 11:00:40 +0530

    Update RegexClass.java

--- Assignment1/RegexClass.java
+++ Assignment1/RegexClass.java
@@ -2,11 +2,11 @@ import java.util.*;
 import java.io.*;
 import java.util.regex.Pattern;
 /**
-*A program to search through the home directory and look for files that match given regular expression. 
-*It takes inputs repeatedly based on number of times to search and prints out the full absolute path 
-*of the matching files found and implemented Exception Handling to handle null pointer Exception.
-*/
-public class RegexClass {
+ *A program to search through the home directory and look for files that match given regular expression.
+ *It takes inputs repeatedly based on number of times to search and prints out the full absolute path
+ *of the matching files found and implemented Exception Handling to handle null pointer Exception.
+ */
+public class RegexClass{
 
     public static void checkForFiles(String regex, String filePath) {
         try {
@@ -20,9 +20,9 @@ public class RegexClass {
                 // if we find a directory call the method with the subDirectory name and same regular expression
                 if (isFile.isDirectory())
                 {
-                      // if(isFile.list().length>-1) {
-                        checkForFiles(regex, isFile.getAbsolutePath());
-                      //  }
+                    // if(isFile.list().length>-1) {
+                    checkForFiles(regex, isFile.getAbsolutePath());
+                    //  }
                 }
                 else {
                     //If its a file we print the absolute path
@@ -31,31 +31,30 @@ public class RegexClass {
                         System.out.println(isFile.getAbsolutePath());
                 }
             }
-        } 
+        }
         //handle the empty file/directory's NullPointerException
-        catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
+        catch (NullPointerException exception) {
+            exception.printStackTrace();
+            System.out.println("NullPointerException Caught!");
         }
     }
-            public static void main(String[] args)
-            {
+    public static void main(String[] args)
+    {
 
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Enter how many times to run :");
-                //This helps to take input recursively and stop after several times, in this way it avoid StackOverFlow
-                int times = scanner.nextInt();
-                while (times-- >0) {
-                    // Assignments Directory
-                   // findMatchingFiles(".*/*.java","/home/sunandini/JavaAssignments");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the regular expression :");
+        String regex = scanner.nextLine();
+        String filepath ="/home";
+        System.out.println("Enter how many times to run :");
+        //This helps to take input recursively and stop after several times, in this way it avoid StackOverFlow
+        int times = scanner.nextInt();
+        while (times-- >0) {
+            // Assignments Directory
+            // findMatchingFiles(".*/*.java","/home/sunandini/JavaAssignments");
 
-                    // home directory
-                   checkForFiles(".*/*.java", "/home");
-
-                }
-            }
+            checkForFiles(regex, filepath);
 
         }
+    }
 
-
-
+}
