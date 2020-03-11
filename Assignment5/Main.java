dcba8752d02b41fb9b037ede2476dfa9bea7af7e
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 21:44:07 +0530

    Update Main.java

--- Assignment5/Main.java
+++ Assignment5/Main.java
@@ -3,7 +3,10 @@ import sunandini.assignment.data.*;
 import sunandini.assignment.singleton.*;
 
 public class Main {
-
+/**
+*Java function creating object of first class and call both the methods to print the values
+*creating object of second class using static method and then call the other method to print the String
+*/
 	public static void main(String[] args) {
 
 		Data data = new Data();
