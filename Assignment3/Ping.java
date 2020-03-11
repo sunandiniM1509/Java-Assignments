fbdb10efedfb39f8fea8166c8fbe8c49d32c97c4
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 21:16:09 +0530

    Update Ping.java

--- Assignment3/Ping.java
+++ Assignment3/Ping.java
@@ -1,45 +1,54 @@
 import java.util.*;
 import java.io.*;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
 public class Ping
 {
-
-        //function that will ping any host ( given as input ) and computes the median of the time taken to ping
-        public static void storePingResponse (String command) throws IOException {
-            try {
-                Process process = Runtime.getRuntime().exec(command);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                ArrayList<Float> pingtime = new ArrayList<>();
-                String response = "";
-                while ((response = reader.readLine()) != null) {
-                    if (Pattern.matches(".*time=.*", response)) {
-                        //  example response : "64 bytes from maa03s26-in-f14.1e100.net (172.217.167.142): icmp_seq=3 ttl=57 time=18.6 ms"
-                        String segments[] = response.split(" ");
-                        //[time=18.6] to 18.6
-                        String pingSeconds = segments[segments.length - 2].substring(5);
-                        System.out.println(response);
-                        pingtime.add(Float.parseFloat(pingSeconds));
-                    }
+    private static Logger logger = Logger.getLogger(RegexClass.class.getName());
+    /**
+     * Java function that will ping any host and computes the median of the time taken to ping.
+     * The system ping utility is used.
+     * @param command String to hold the utility command passed through main method
+     * @throws IndexOutOfBoundsException for trying to access an invalid host
+     * @throws IOException for handling readLine() and exec() methods
+     */
+    public static void storePingResponse (String command)  {
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            ArrayList<Float> pingtime = new ArrayList<>();
+            String response = "";
+            while ((response = reader.readLine()) != null) {
+                if (Pattern.matches(".*time=.*", response)) {
+                    String segments[] = response.split(" ");
+                    String pingSeconds = segments[segments.length - 2].substring(5);
+                    logger.info(response);
+                    pingtime.add(Float.parseFloat(pingSeconds));
                 }
-                Collections.sort(pingtime);
-                int length = pingtime.size();
-                if (length % 2 == 0)
-                    //Median of the pingtime ArrayList
-                    System.out.println("Median of the time taken to ping = " + (pingtime.get(length / 2) + pingtime.get(length / 2 - 1)) / 2 + " ms");
-                else
-                    System.out.println("Median of the time take to ping = " + pingtime.get(length / 2) + " ms");
             }
-
-            catch (Exception exception)
-            {
-                exception.printStackTrace();
-            }
+            Collections.sort(pingtime);
+            int length = pingtime.size();
+            if (length % 2 == 0)
+                logger.info("Median of the time taken to ping = " + (pingtime.get(length / 2) + pingtime.get(length / 2 - 1)) / 2 + " ms");
+            else
+                logger.info("Median of the time take to ping = " + pingtime.get(length / 2) + " ms");
+        }
+        catch (IndexOutOfBoundsException exception)
+        {
+            exception.printStackTrace();
+            logger.info("Caught IndexOutOfBounds exception");
+        }
+        catch (IOException exception2)
+        {
+            exception2.printStackTrace();
+            logger.info("Caught IOException");
+        }
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        System.out.print("url of the host is : ");
+        logger.info("url of the host is : ");
         String url = scanner.nextLine();
-        System.out.print("number of times to ping :");
+        logger.info("number of times to ping :");
         int pingCount = scanner.nextInt();
         storePingResponse("ping -c" + pingCount + " " + url);
     }
