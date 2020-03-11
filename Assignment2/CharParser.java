9a723a59c0596d3dd7929ff540f1017d9bca5937
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-03-11 21:18:49 +0530

    Update CharParser.java

--- Assignment2/CharParser.java
+++ Assignment2/CharParser.java
@@ -1,17 +1,18 @@
 import java.util.*;
 public class CharParser {
-    //A java function to check if the input string contains all the letters of the alphabet a-z (case-insensitive)
+    /**
+    *A java function to check if the input string contains all the letters of the alphabet a-z (case-insensitive)
+    *@param input String to hold user provided input
+    */
     static boolean checkCharacter(String input)
     {
         if(input.length()<26)
             return false;
-        // count array to hold+count the occurrences of a-z characters.
         int[] countchar = new int[26];
         int length = input.length();
         
         for(int i=0;i<length;i++)
             countchar[input.charAt(i)-'a']++;
-        //if the occurrence of atleast one character is less than 1(i.e., if it does not appear) return false.
         for(int j=0;j<26;j++)
         {
             if(countchar[j]<1)
@@ -28,5 +29,7 @@ public class CharParser {
     System.out.println("No, The given input String does not consists of all the {a-z} characters");        
     }
 }
-// The algorithm has Linear Time Complexity of O(n) where n is the length of the string.
-// Space Complexity is O(26) as a constant array of size 26 (count lower case alphabets ) is required to store occurrences of a-z.
+/**
+*The algorithm has Linear Time Complexity of O(n) where n is the length of the string.
+*Space Complexity is O(26) as a constant array of size 26 (count lower case alphabets ) is required to store occurrences of a-z.
+*/
