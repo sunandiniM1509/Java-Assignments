f15c1236ce5db20d9d3e1d53a41da4c127ed0e26
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-04-17 16:51:10 +0530

    Update RegexCaps.java

--- Assignment9/RegexCaps.java
+++ Assignment9/RegexCaps.java
@@ -1,21 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.io.*;
 /**
-*Using java.util.regex.Pattern as a resource, a function is written to 
-*check if a sentence begins with a capital letter and ends with a period.
-*/
+ *Using java.util.regex.Pattern as a resource, a function is written to
+ *check if a sentence begins with a capital letter and ends with a period.
+ */
 class RegexCaps
 {
     static Logger logger = Logger.getLogger(RegexCaps.class.getName());
     public static void main(String args[])throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String regex = "^[A-Z].*[.]";
         String input = reader.readLine();
-        if(Pattern.matches(regex,input))
+        if(checkConstraints(input))
             logger.info("Yes, the string starts with a Capital letter and ends with a '.' ");
         else
             logger.info("No, the string does not start with a Capital letter and end with a '.' ");
+    }
+    public static boolean checkConstraints(String input)
+    {
+        String regex = "^[A-Z].*[.]";
+        if(Pattern.matches(regex,input))
+        {
+            return true;
+        }
+        return false;
     }
 }
