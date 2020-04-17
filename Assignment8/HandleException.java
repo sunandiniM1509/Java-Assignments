cdd9e7089c4eb8828575a3aac0b082fefd2cff26
sunandiniM1509 <47917036+sunandiniM1509@users.noreply.github.com>
2020-04-17 16:18:24 +0530

    Update HandleException.java

--- Assignment8/HandleException.java
+++ Assignment8/HandleException.java
@@ -1,13 +1,13 @@
 
 /**
-*This program creates three custom Exceptions and a Handler class to throw all three. 
-*A finally block is added to verify if the clause is executed.
-*/
+ *This program creates three custom Exceptions and a Handler class to throw all three.
+ *A finally block is added to verify if the clause is executed.
+ */
 class UserException1 extends Exception
 {
     public UserException1(String string1)
     {
-        System.out.println(string1);
+        super(string1);
     }
 }
 
@@ -15,7 +15,7 @@ class UserException2 extends Exception
 {
     public UserException2(String string2)
     {
-        System.out.println(string2);
+        super(string2);
     }
 }
 
@@ -23,7 +23,7 @@ class UserException3 extends Exception
 {
     public UserException3(String string3)
     {
-        System.out.println(string3);
+        super(string3);
     }
 }
 class Handler {
@@ -38,7 +38,7 @@ class Handler {
                     throw new UserException3("User Defined Exception 3");
             }
         } catch (Exception exception) {
-            System.out.println("User Defined Exception caught");
+            System.out.println("User Defined Exception is caught");
         } finally {
             System.out.println("Finally block is executed");
             System.out.println();
@@ -52,6 +52,3 @@ public class HandleException {
         Handler.handlingThrown(3);
     }
 }
-
-
-
